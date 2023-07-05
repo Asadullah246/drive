cfa44fd556faa69c206d65400a6ee817d92ac33f
MirasUddin <mirasuddin978@gmail.com>
2023-07-05 16:51:50 +0600

    mydoc update

--- src/components/MyOwn.jsx
+++ src/components/MyOwn.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 // import { useState } from "react";
 
 const MyOwn = () => {
-    const [value, setValue] = useState(2);
+    const [value, setValue] = useState(5);
     return (
         <div>
             <div className="overflow-x-auto">
@@ -26,7 +26,7 @@ const MyOwn = () => {
                                         <div className="font-bold">Hart Hagerty</div>
                                         <div className="flex">
                                             <div>
-                                                <h3>Best seller: </h3>
+                                                <h3>Best seller: Asadullah</h3>
                                             </div>
                                             <div className="text-sm opacity-50">Total Hours: 10</div>
                                         </div>
@@ -34,32 +34,103 @@ const MyOwn = () => {
                                 </div>
                             </td>
                             <td>
-                            <Rating name="read-only" value={value} readOnly
+                                <Rating name="read-only" value={value} readOnly
                                 // onChange={(event, newValue) => {
                                 //     setValue(newValue);
                                 // }}
                                 />
                             </td>
                             <td>
-                                <GroupIcon/>
+                                <div className="flex justify-center align-middle">
+                                    <GroupIcon />
+                                    <h2>500</h2>
+                                </div>
                             </td>
                             <th>
-                                <FavoriteBorderIcon/>
+                                <div>
+                                    <button> <FavoriteBorderIcon /></button>
+                                </div>
+                            </th>
+                        </tr>
+                        {/* row 2 */}
+                        <tr>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <div className="mask w-12 h-12">
+                                            <img src={image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">Hart Hagerty</div>
+                                        <div className="flex">
+                                            <div>
+                                                <h3>Best seller: Asadullah</h3>
+                                            </div>
+                                            <div className="text-sm opacity-50">Total Hours: 10</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <Rating name="read-only" value={value} readOnly
+                                // onChange={(event, newValue) => {
+                                //     setValue(newValue);
+                                // }}
+                                />
+                            </td>
+                            <td>
+                                <div className="flex justify-center align-middle">
+                                    <GroupIcon />
+                                    <h2>500</h2>
+                                </div>
+                            </td>
+                            <th>
+                                <div>
+                                    <button> <FavoriteBorderIcon /></button>
+                                </div>
+                            </th>
+                        </tr>
+                        {/* row 3 */}
+                        <tr>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <div className="mask w-12 h-12">
+                                            <img src={image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">Hart Hagerty</div>
+                                        <div className="flex">
+                                            <div>
+                                                <h3>Best seller: Asadullah</h3>
+                                            </div>
+                                            <div className="text-sm opacity-50">Total Hours: 10</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <Rating name="read-only" value={value} readOnly
+                                // onChange={(event, newValue) => {
+                                //     setValue(newValue);
+                                // }}
+                                />
+                            </td>
+                            <td>
+                                <div className="flex justify-center align-middle">
+                                    <GroupIcon />
+                                    <h2>500</h2>
+                                </div>
+                            </td>
+                            <th>
+                                <div>
+                                    <button> <FavoriteBorderIcon /></button>
+                                </div>
                             </th>
                         </tr>
                     </tbody>
-
-                    {/* foot */}
-                    <tfoot>
-                        <tr>
-                            {/* <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th> */}
-                        </tr>
-                    </tfoot>
-
                 </table>
             </div>
         </div>
