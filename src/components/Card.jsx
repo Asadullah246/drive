01135fa97355a69c56d7017a0e9afa0673e450a8
MirasUddin <mirasuddin978@gmail.com>
2023-07-05 11:01:38 +0600

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,47 +2,44 @@ import cardImg1 from '../assets/CardImage/shop2.jpg'
 
 const Card = () => {
     return (
-        <div className="grid lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1">
-            <div>
-                <h2>This is card</h2>
+        <div className="grid lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1 justify-center">
+            <div className="mb-8">
                 <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src={cardImg1} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
+                    <figure><img src={cardImg1} alt="Shoes" /></figure>
+                    <div className="card-body">
                         <h2 className="card-title">Shoes!</h2>
+                        <h3>Author: Asadullah</h3>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
+                        <p>Rating: 5 star</p>
+                        <div className="card-actions justify-start">
                             <button className="btn btn-primary">Buy Now</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div>
-                <h2>This is card</h2>
+            <div className="mb-8">
                 <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src={cardImg1} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
+                    <figure><img src={cardImg1} alt="Shoes" /></figure>
+                    <div className="card-body">
                         <h2 className="card-title">Shoes!</h2>
+                        <h3>Author: Asadullah</h3>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
+                        <p>Rating: 5 star</p>
+                        <div className="card-actions justify-start">
                             <button className="btn btn-primary">Buy Now</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div>
-                <h2>This is card</h2>
+            <div className="mb-8">
                 <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src={cardImg1} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
+                    <figure><img src={cardImg1} alt="Shoes" /></figure>
+                    <div className="card-body">
                         <h2 className="card-title">Shoes!</h2>
+                        <h3>Author: Asadullah</h3>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
+                        <p>Rating: 5 star</p>
+                        <div className="card-actions justify-start">
                             <button className="btn btn-primary">Buy Now</button>
                         </div>
                     </div>
