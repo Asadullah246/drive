3d8c9a8b00116af02475c30be7745fd71ec6b5b9
MirasUddin <mirasuddin978@gmail.com>
2023-07-05 11:49:43 +0600

    check added

--- src/pages/home/ProductDetails.jsx
+++ src/pages/home/ProductDetails.jsx
@@ -1,3 +1,4 @@
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 const ProductDetails = () => {
     return (
@@ -11,6 +12,7 @@ const ProductDetails = () => {
                             <h1 className="text-5xl font-bold">Box Office News!</h1>
                             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                             <button className="btn btn-primary">Get Started</button>
+                            <CheckCircleOutlineIcon style={{color:"#1976D2"}}/>
                         </div>
                     </div>
                 </div>
