caa4cb145391853ba17d3751cab60a3aee0a8387
MirasUddin <mirasuddin978@gmail.com>
2023-07-05 12:56:23 +0600

    Product Details page Update

--- src/pages/home/ProductDetails.jsx
+++ src/pages/home/ProductDetails.jsx
@@ -2,6 +2,8 @@
 import LearningDetails from '../../components/LearningDetails';
 import image from "../../assets/CardImage/download.jpg"
 import { Rating } from "@mui/material";
+import { useState } from 'react';
+import { LoadingButton } from '@mui/lab';
 
 const ProductDetails = () => {
     const [value, setValue] = useState(2);
@@ -14,13 +16,18 @@ const ProductDetails = () => {
                     <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
                         <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p className="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aliquam porro, iste dolore ratione quasi voluptatibus, obcaecati pariatur doloremque amet ab maiores esse consequuntur dolor, voluptas aliquid sequi atque in repellat nisi id molestias architecto corporis alias? Reprehenderit, ducimus minus laudantium nostrum, ullam nulla, pariatur cupiditate recusandae similique magni eius?</p>
                         <Rating name="read-only" value={value} readOnly
-                                // onChange={(event, newValue) => {
-                                //     setValue(newValue);
-                                // }}
-                                />
-                        <button className="btn btn-primary">Get Started</button>
+                        // onChange={(event, newValue) => {
+                        //     setValue(newValue);
+                        // }}
+                        />
+                        <p>Lates Update: 3/2023</p>
+                        <div className="flex mt-2">
+                            <LoadingButton loading={false} variant="contained">
+                                Submit
+                            </LoadingButton>
+                        </div>
                     </div>
                 </div>
             </div>
