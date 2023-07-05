21ba4dbf85cc8a7c7da5d3feefd49eadc3c44992
Asadullah246 <asadm2258@gmail.com>
2023-07-05 11:00:43 +0600

    card updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,55 +1,36 @@
-import cardImg1 from '../assets/CardImage/shop2.jpg'
+import cardImg1 from "../assets/CardImage/download.jpg";
 
 const Card = () => {
-    return (
-        <div className="grid lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1">
-            <div>
-                <h2>This is card</h2>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src={cardImg1} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
+  return (
+    <div className="grid lg:grid-cols-4  md:grid-cols-2 sm:grid-cols-1 gap-4 md:gap-8 ">
+     {
+        [...Array(6)].map((a,index)=>{
+            return (
+                <div key={index} className="card  bg-base-100 shadow-xl">
+                <figure>
+                  <img
+                    src={cardImg1}
+                    alt="Shoes"
+                    className="w-full "
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    Shoes!
+                    <div className="badge badge-secondary">NEW</div>
+                  </h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <div className="card-actions justify-start">
+                    <div className="badge badge-outline">Fashion</div>
+                    {/* <div className="badge badge-outline">Products</div> */}
+                  </div>
                 </div>
-            </div>
-            <div>
-                <h2>This is card</h2>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src={cardImg1} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <h2>This is card</h2>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src={cardImg1} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+              </div>
+            )
+        })
+     }
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/home/Dashboard.jsx
+++ src/pages/home/Dashboard.jsx
@@ -9,6 +9,10 @@ const Dashboard = () => {
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col p-6 mt-8 ">
+            <div className='w-full flex justify-start items-center mb-6 gap-6'>
+                <input type="text" name="" id="" className='w-1/2 p-2 border-2 rounded-md ' placeholder='search here' />
+                <button className='border-2 rounded-md '>Search</button>
+            </div>
           <Outlet></Outlet>
           <label
             htmlFor="my-drawer-2"
@@ -19,7 +23,7 @@ const Dashboard = () => {
         </div>
         <div className="drawer-side  ">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 h-full bg-[#1976D2] text-white"> 
+          <ul className="menu p-4 w-80 h-full bg-purple-900  text-white">
             {/* Sidebar content here */}
 
             <li>
