8a9df86eee2ff74c786d37f34be59d7e661b9a42
Asadullah246 <asadm2258@gmail.com>
2023-07-05 09:34:19 +0600

    dashboard added

---
+++ src/pages/home/Category1.jsx
@@ -0,0 +1,11 @@
+
+
+const Category1 = () => {
+    return (
+        <div>
+this is category 1
+        </div>
+    );
+};
+
+export default Category1;
---
+++ src/pages/home/Dashboard.jsx
@@ -0,0 +1,39 @@
+
+import { Link, Outlet } from 'react-router-dom';
+
+const Dashboard = () => {
+    return (
+        <div>
+             {/* dashboard  */}
+
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          <Outlet></Outlet>
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side  ">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-[#0084FF] text-white">
+            {/* Sidebar content here */}
+
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/category-1">Category 1</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+        </div>
+    );
+};
+
+export default Dashboard;
