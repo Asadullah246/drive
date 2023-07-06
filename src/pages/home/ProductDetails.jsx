052980efcf04f9d6510fe8711d142f80036febf2
MirasUddin <mirasuddin978@gmail.com>
2023-07-06 09:20:23 +0600

    product page update

--- src/pages/home/ProductDetails.jsx
+++ src/pages/home/ProductDetails.jsx
@@ -4,29 +4,53 @@ import image from "../../assets/CardImage/download.jpg"
 import { Rating } from "@mui/material";
 import { useState } from 'react';
 import { LoadingButton } from '@mui/lab';
+import testImage from "../../assets/CardImage/download.jpg";
+
+const data =
+{
+    id: 1,
+    name: "product 1",
+    category: "cat 1",
+    sub_category: "sub cat-10",
+    sub_sub_category: "sub sub cat-1",
+    price: 300,
+    uploader: "miras",
+    description: "this is a test product",
+    review: [],
+    image: testImage,
+    learning_subject: [],
+    total_view: 10,
+
+    // ami add korlam 
+    regular_price: 500,
+    status: 'In Stock',
+}
+
+
 
 const ProductDetails = () => {
     const [value, setValue] = useState(2);
+
     return (
-        <div>
-            <LearningDetails />
-            <h2>This is product details page</h2>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
+        <div className='max-w-75vh'>
+            {/* <LearningDetails /> */}
+            <div>
+                <div className='flex'>
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aliquam porro, iste dolore ratione quasi voluptatibus, obcaecati pariatur doloremque amet ab maiores esse consequuntur dolor, voluptas aliquid sequi atque in repellat nisi id molestias architecto corporis alias? Reprehenderit, ducimus minus laudantium nostrum, ullam nulla, pariatur cupiditate recusandae similique magni eius?</p>
-                        <Rating name="read-only" value={value} readOnly
-                        // onChange={(event, newValue) => {
-                        //     setValue(newValue);
-                        // }}
-                        />
-                        <p>Lates Update: 3/2023</p>
-                        <div className="flex mt-2">
-                            <LoadingButton loading={false} variant="contained">
-                                Submit
-                            </LoadingButton>
+                        <img className='w-80 me-8' src={data.image} alt="product image" />
+                    </div>
+                    <div className='ms-3'>
+                        <h2 className='text-4xl '>{data.name}</h2>
+                        <div className='lg:flex mt-4'>
+                            <h3 className='bg-slate-200 rounded-full px-2 mx-1 mt-2'>Price: <span className='text-black font-semibold'>{data.price} ৳</span></h3>
+                            <h3 className='bg-slate-200 rounded-full px-2 mx-1 mt-2'>Uploader: <span className='text-black font-semibold'>{data.uploader} ৳</span></h3>
+                            <h3 className='bg-slate-200 rounded-full px-2 mx-1 mt-2'>Status: <span className='text-black font-semibold'>{data.status}</span></h3>
+                            <h3 className='bg-slate-200 rounded-full px-2 mx-1 mt-2'>Category: <span className='text-black font-semibold'>{data.category}</span></h3>
+                            <h3 className='bg-slate-200 rounded-full px-2 mx-1 mt-2'>Sub Category: <span className='text-black font-semibold'>{data.sub_category}</span></h3>
+                        </div>
+                        <div className='mt-4'>
+                            <h2 className='text-xl'>Key Features</h2>
+                            <p>Model: </p>
                         </div>
                     </div>
                 </div>
